21e485daa3d0f0ede29320066dded2c0620a4840
Mike Campbell <mlcampbell@gmail.com>
2018-02-08 11:13:43 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tea Wallpaper
+A Ubersicht widget to display a wallpaper with colors that correspond to the time of the day. The script cycles through 8 images and blends
+them together seemlessly so that there is a smooth transition between the images over a 3 hour period.
+
+# Install
+Edit the teawallpaper.js and review/modify the following items:
+1) The "firstHourImage" variable on line 50. This is the time of day that the first images in the "images" array will be displayed. By default this is 6am.
+2) The "imageDir" variable on line 51. This is the subdirectory that holds the 8 images to cycle through. The default is "hexagons" but the widget also includes "solids", "triangles", and "trianglify" to try out as well.
+
+# Customization
+A new set of images can be created to cycle through by creating 8 images with the names "sunrise.png", "day.png", "afternoon.png",
+"sunset.png", "dusk.png", "night.png", "twilight.png", "morning.png". The widget will transition between the images every 3 hours over a
+24 hour day.
+
+Take a look at the live geopattern generator at http://btmills.github.io/geopattern to get started with some ideas.
+
+# Screenshot
+It is hard to build a screenshot as the background changes over time. The below screenshot shows the color as a complete 24-hour cycle
+from 6am to 6am. When loaded the progression between colors is updated every 10 minutes so that the transition between 2 images is hardly
+noticable other than the color will gradually change.
+![Alt text](/screenshot.png?raw=true)
+
+# Credits
+This code is based upon the original source code for Tea-Time-Lapse by gruppler at https://gruppler.deviantart.com/art/Tea-Time-Lapse-99395260
+
+The geometry SVG patterns are created with the geopattern background generator at https://github.com/btmills/geopattern which is a JavaScript port of http://jasonlong.github.io/geo_pattern.
---
+++ teawallpaper.js
@@ -0,0 +1,97 @@
+command: '',
+
+refreshFrequency: '10m',
+
+style: '\n\
+  left: 0px;\n\
+  top: 0px;\n\
+  height: 100%;\n\
+  width: 100%;\n\
+  position: absolute;\n\
+  div.bottomLayerContainer {\n\
+    z-index: -10000;\n\
+    position: absolute;\n\
+    top: 0px;\n\
+    left: 0px;\n\
+    width: 100%;\n\
+    height: 100%;\n\
+    background-size: cover;\n\
+  };\n\
+  div.topLayerContainer {\n\
+    z-index: -9999;\n\
+    position: absolute;\n\
+    top: 0px;\n\
+    left: 0px;\n\
+    width: 100%;\n\
+    height: 100%;\n\
+    background-size: cover;\n\
+  }\n\
+',
+
+render: function(_) {
+  return '<div class="bottomLayerContainer"></div><div class="topLayerContainer"></div>';
+},
+
+afterRender: function(domEl) {
+  this.currIndex = -1;
+},
+
+update: function(output, domEl) {
+  var images = [
+    "sunrise.png",
+    "day.png",
+    "afternoon.png",
+    "sunset.png",
+    "dusk.png",
+    "night.png",
+    "twilight.png",
+    "morning.png"
+  ];
+  var firstImageHour = 6;
+  var imageDir = "hexagons";
+  var anHour = 60 * 60 * 1000;
+  var imageCount = images.length;
+  var interval = 24 / imageCount;
+  var intervalMS = interval * anHour;
+  var offsetMS = firstImageHour * anHour;
+  var time = new Date();
+  var timestamp = time % (60000) + time.getHours() * anHour + time.getMinutes() * 60000;
+  var index = (imageCount + Math.floor((new Date().getHours() - firstImageHour) / interval)) % imageCount;
+  var opacity = (Math.abs(timestamp - offsetMS % intervalMS) % intervalMS) / intervalMS;
+
+  var $domEl = $(domEl);
+  var bottomLayer = new Image();
+  var topLayer = new Image();
+  // bottom layer
+  bottomLayer.onload = function() {
+    console.log("bottom layer=" + bottomLayer.src);
+    $domEl.find('.bottomLayerContainer').addClass('old');
+    var $div = $('<div class="bottomLayerContainer" />');
+    $div.css('background-image', 'url("' + bottomLayer.src + '")');;
+	$div.css('display', 'none');
+    $div.fadeIn('slow', function () {
+      $domEl.find('.old').remove();
+    });
+    $domEl.append($div);
+  };
+  // top layer
+  topLayer.onload = function() {
+    console.log("top layer=" + topLayer.src + " opacity=" + opacity);
+    $domEl.find('.topLayerContainer').addClass('old');
+    var $div = $('<div class="topLayerContainer" />');
+    $div.css('background-image', 'url("' + topLayer.src + '")');
+    $div.css('opacity', opacity);
+	$div.css('display', 'none');
+    $div.fadeIn(10, function () {
+      $domEl.find('.old').remove();
+    });
+    $domEl.append($div);
+  };
+  if (this.currIndex != index) {
+    console.log("updating bottom layer");
+    this.currIndex = index;
+    bottomLayer.src = "teawallpaper.widget/" + imageDir + "/" + images[index];
+  }
+  topLayer.src = "teawallpaper.widget/" + imageDir + "/" + images[(index + 1) % imageCount];
+}
+
